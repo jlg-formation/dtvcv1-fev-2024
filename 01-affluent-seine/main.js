ab29c0534ba999560d86c9772e22a46d15237005
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-02-21 15:21:09 +0100

    dev: added source

--- 01-affluent-seine/main.js
+++ 01-affluent-seine/main.js
@@ -1,8 +1,31 @@
-var chartDom = document.querySelector("div.content");
-var myChart = echarts.init(chartDom);
-var option;
+/** @type HTMLElement | null */
+const chartDom = document.querySelector("div.content");
+if (chartDom === null) {
+  throw new Error("div.content not found");
+}
 
-option = {
+const source = [
+  ["name", "longueur"],
+  ["Marne", 514],
+  ["Oise", 341.1],
+  ["Yonne", 292.3],
+  ["Aube", 248.3],
+  ["Eure", 228.7],
+  ["Risle", 145],
+  ["Loing", 142.7],
+  ["Epte", 113],
+  ["Ource", 100.4],
+  ["Yerres", 97.5],
+  ["Essonne", 97.1],
+  ["Andelle", 56.8],
+  ["Barse", 50.1],
+  ["Orge", 50.1],
+  ["Voulzie", 43.9],
+  ["Almont", 42.1],
+  ["Orvin", 38.1],
+];
+const myChart = echarts.init(chartDom);
+const option = {
   dataset: {
     source: [
       ["score", "amount", "product"],
