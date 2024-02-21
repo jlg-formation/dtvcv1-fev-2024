4c7f3e4fce705879afec8da84643f27de492845d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-02-21 15:32:33 +0100

    dev: add diagram bar

--- 01-affluent-seine/main.js
+++ 01-affluent-seine/main.js
@@ -5,7 +5,6 @@ if (chartDom === null) {
 }
 
 const source = [
-  ["name", "longueur"],
   ["Marne", 514],
   ["Oise", 341.1],
   ["Yonne", 292.3],
@@ -23,46 +22,26 @@ const source = [
   ["Voulzie", 43.9],
   ["Almont", 42.1],
   ["Orvin", 38.1],
+  ["name", "longueur"],
 ];
+source.reverse();
+
 const myChart = echarts.init(chartDom);
 const option = {
   dataset: {
-    source: [
-      ["score", "amount", "product"],
-      [89.3, 58212, "Matcha Latte"],
-      [57.1, 78254, "Milk Tea"],
-      [74.4, 41032, "Cheese Cocoa"],
-      [50.1, 12755, "Cheese Brownie"],
-      [89.7, 20145, "Matcha Cocoa"],
-      [68.1, 79146, "Tea"],
-      [19.6, 91852, "Orange Juice"],
-      [10.6, 101852, "Lemon Juice"],
-      [32.7, 20112, "Walnut Brownie"],
-    ],
+    source: source,
   },
   grid: { containLabel: true },
-  xAxis: { name: "amount" },
+  xAxis: { name: "name" },
   yAxis: { type: "category" },
-  visualMap: {
-    orient: "horizontal",
-    left: "center",
-    min: 10,
-    max: 100,
-    text: ["High Score", "Low Score"],
-    // Map the score column to color
-    dimension: 0,
-    inRange: {
-      color: ["#65B581", "#FFCE34", "#FD665F"],
-    },
-  },
   series: [
     {
       type: "bar",
       encode: {
         // Map the "amount" column to X axis.
-        x: "amount",
+        x: "longueur",
         // Map the "product" column to Y axis
-        y: "product",
+        y: "name",
       },
     },
   ],
