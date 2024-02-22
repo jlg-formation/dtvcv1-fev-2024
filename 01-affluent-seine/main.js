1634d6eb099c65c887f60190101d8996992de994
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-02-22 10:27:50 +0100

    dev: added transition on rectangle

--- 01-affluent-seine/main.js
+++ 01-affluent-seine/main.js
@@ -20,6 +20,7 @@ const source = [
 ];
 
 const height = 1.8;
+const duration = 1000;
 
 d3.select("div.content")
   .selectAll("div.name")
@@ -30,7 +31,7 @@ d3.select("div.content")
   .style("opacity", 0)
   .text((d) => d[0])
   .transition()
-  .duration(2000)
+  .duration(duration)
   .style("opacity", 1);
 
 d3.select("div.content")
@@ -38,6 +39,11 @@ d3.select("div.content")
   .data(source)
   .join("div")
   .classed("bar", true)
-  .style("width", (d) => `${d[1] / 16}em`)
+  .style("width", 0)
   .style("transform", (d, i) => `translate(10.5em, ${i * height}em)`)
-  .text((d) => d[1]);
+  .text((d) => d[1])
+  .style("opacity", 0)
+  .transition()
+  .duration(duration)
+  .style("width", (d) => `${d[1] / 16}em`)
+  .style("opacity", 1);
