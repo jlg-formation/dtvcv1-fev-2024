538de82d4f685318e1a8863563734d5472938763
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-02-22 11:38:28 +0100

    dev: added debit on dataviz

--- 01-affluent-seine/main.js
+++ 01-affluent-seine/main.js
@@ -1,37 +1,50 @@
 // @ts-nocheck
 const main = async () => {
   const csv = await d3.dsv(",", "./query.csv");
-  const source = csv.map((line) => [line.riverLabel, line.length, line.debit]);
-  console.log("source: ", source);
 
-  const height = 1.8;
-  const duration = 1000;
+  const button = document.querySelector("button.switch");
 
-  d3.select("div.content")
-    .selectAll("div.name")
-    .data(source)
-    .join("div")
-    .classed("name", true)
-    .style("transform", (d, i) => `translate(0, ${i * height}em)`)
-    .style("opacity", 0)
-    .text((d) => d[0])
-    .transition()
-    .duration(duration)
-    .style("opacity", 1);
+  let state = "length";
+  button.addEventListener("click", () => {
+    state = state === "length" ? "debit" : "length";
+    const source = csv.map((line) => [line.riverLabel, line[state]]);
+    console.log("source: ", source);
+    update(source);
+  });
 
-  d3.select("div.content")
-    .selectAll("div.bar")
-    .data(source)
-    .join("div")
-    .classed("bar", true)
-    .style("width", 0)
-    .style("transform", (d, i) => `translate(10.5em, ${i * height}em)`)
-    .text((d) => d[1])
-    .style("opacity", 0)
-    .transition()
-    .duration(duration)
-    .style("width", (d) => `${d[1] / 16}em`)
-    .style("opacity", 1);
+  const update = (source) => {
+    const height = 1.8;
+    const duration = 1000;
+
+    d3.select("div.content")
+      .selectAll("div.name")
+      .data(source)
+      .join("div")
+      .classed("name", true)
+      .style("transform", (d, i) => `translate(0, ${i * height}em)`)
+      .style("opacity", 0)
+      .text((d) => d[0])
+      .transition()
+      .duration(duration)
+      .style("opacity", 1);
+
+    d3.select("div.content")
+      .selectAll("div.bar")
+      .data(source)
+      .join("div")
+      .classed("bar", true)
+      .style("width", 0)
+      .style("transform", (d, i) => `translate(10.5em, ${i * height}em)`)
+      .text((d) => d[1])
+      .style("opacity", 0)
+      .transition()
+      .duration(duration)
+      .style("width", (d) => `${d[1] / 16}em`)
+      .style("opacity", 1);
+  };
+
+  const source = csv.map((line) => [line.riverLabel, line[state]]);
+  update(source);
 };
 
 main();
